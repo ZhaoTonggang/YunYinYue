397dba997c7b9e3c63a59589c8a54d29fb69f8af
root <root@cvm-3htziv528a225.novalocal>
2021-04-18 23:44:48 +0800

    初次提交

---
+++ precache-manifest.f9d7537efa0fcbe92a6fea1bf265df27.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2951a94024bea31f88ecacbc0eac14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3752e1be27bbca726718",
+    "url": "/static/css/5.09700f6a.chunk.css"
+  },
+  {
+    "revision": "bcab8f7337504d28d159",
+    "url": "/static/css/main.116386cd.chunk.css"
+  },
+  {
+    "revision": "5e0a855bd7de355097fd",
+    "url": "/static/js/2.a79ea46c.chunk.js"
+  },
+  {
+    "revision": "848c4b25d20151f0be4d",
+    "url": "/static/js/3.f76f04ff.chunk.js"
+  },
+  {
+    "revision": "cb1d49a119a1e2e5bc21",
+    "url": "/static/js/4.00de5afc.chunk.js"
+  },
+  {
+    "revision": "3752e1be27bbca726718",
+    "url": "/static/js/5.29133762.chunk.js"
+  },
+  {
+    "revision": "42fee9fbc27fb7e4fdbc",
+    "url": "/static/js/6.54861801.chunk.js"
+  },
+  {
+    "revision": "9cc09c46135ceb950f2b",
+    "url": "/static/js/7.7c7e9114.chunk.js"
+  },
+  {
+    "revision": "d18a33069b798ee9e8ee",
+    "url": "/static/js/8.955d48be.chunk.js"
+  },
+  {
+    "revision": "378508a96e7f2a92175c",
+    "url": "/static/js/9.d7aec8c6.chunk.js"
+  },
+  {
+    "revision": "bcab8f7337504d28d159",
+    "url": "/static/js/main.5de379f1.chunk.js"
+  },
+  {
+    "revision": "7600fb38f846648644be",
+    "url": "/static/js/runtime~main.aa699de5.js"
+  },
+  {
+    "revision": "2a5e9e65b0fb5d5eb065ae1648423bcc",
+    "url": "/static/media/disc.2a5e9e65.png"
+  },
+  {
+    "revision": "64444b39f93291cc186672eca04e6a7f",
+    "url": "/static/media/music.64444b39.png"
+  },
+  {
+    "revision": "047d988b55db51aaeb3496805be1a099",
+    "url": "/static/media/needle.047d988b.png"
+  }
+]);
